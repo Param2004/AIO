546b960f21a5a18e009b0bb8478ce2eea7725cc4
Param sharma <kaushik.param2004@gmail.com>
2024-06-05 19:20:57 +0530

    sv

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const errorMiddleware = require("./middleware/error");
-import cors from "cors"
+const cors = require("cors");
 const cookieParser = require('cookie-parser')
 
 const bodyParser = require("body-parser");
